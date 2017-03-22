1c3bbb0da5630d37a3b3f3b2b20632b1edbbf214
mjordan872 <mjordan872@gmail.com>
2017-03-22 21:17:57 +0000

    async i/o challenge

--- program.js
+++ program.js
@@ -1,12 +1,8 @@
 
 var fs = require('fs');
 
-var a = fs.readFileSync(process.argv[2]).toString();
-var result = 0;
 
-for(var i =0; i < a.length; i++){
-    if(a[i] == "\n"){
-        result++ ;
-    }
-}
-console.log(result);
+var a = fs.readFile(process.argv[2], function(err, data){
+    var answer = data.toString().split('\n').length-1;
+    console.log(answer);
+})
